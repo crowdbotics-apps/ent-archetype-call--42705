89ff38b042439bde6c4fe9732b274dfb6dd3cd8a
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2023-08-10 17:38:35 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,11 @@
 export const globalOptions = {
-  name: "ent-archetype-call--42705",
-  url: "https://ent-archetype-call--42705.botics.co",
-  api: "https://ent-archetype-call--42705.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
+  "name": "ent-archetype-call--42705",
+  "url": "https://ent-archetype-call--42705.botics.co",
+  "api": "https://ent-archetype-call--42705.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "ScreenAI10"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
