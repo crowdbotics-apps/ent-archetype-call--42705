4b8325c3f1a09b19c743ae69b2751c6dda6db3ac
crowdbotics-apps <team@crowdbotics.com>
2023-08-11 00:43:36 +0000

    Update models

--- backend/foo/models.py
+++ backend/foo/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class TestModelInFoo(models.Model):
     'Generated Model'
     my_id = models.BigIntegerField()
+    field_two = models.BigIntegerField(null=True,blank=True,)
 
 # Create your models here.
